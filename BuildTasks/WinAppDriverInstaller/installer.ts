5f06a456482d877320e03a19fd0a0d27bf4c4f2e
canhua <licanhua@live.com>
2019-10-04 21:17:32 -0700

    init

---
+++ BuildTasks/WinAppDriverInstaller/installer.ts
@@ -0,0 +1,47 @@
+import * as taskLib from 'azure-pipelines-task-lib/task';
+import * as toolLib from 'azure-pipelines-tool-lib/tool';
+
+import * as util from 'util';
+const exec = util.promisify(require('child_process').exec);
+
+async function execCommand(command: string) {
+  console.log('command: ' + command);
+  const { stdout, stderr } = await exec(command);
+  if (stdout) {
+    console.log('stdout: ' + stdout);
+  }
+  if (stderr) {
+    console.log('stderr: ' + stderr);
+  }
+}
+
+
+async function run() {
+  try {
+    let version = taskLib.getInput('WinAppDriverVersion', false);
+
+    if (!version) {
+      version = 'v1.1';
+    }
+
+    const downloadUrl = `https://github.com/microsoft/WinAppDriver/releases/download/${version}/WindowsApplicationDriver.msi`;
+
+    console.log('Download WinAppDriver from ' + downloadUrl);
+    const msiPath = await toolLib.downloadTool(downloadUrl);
+    console.log('Downloaded File: ' + msiPath);
+    
+    const uninstallCommand = 'msiexec /uninstall {C4903086-429C-4455-86DD-044914BBA07B} /qn'
+    await execCommand(uninstallCommand).catch(() => { });
+
+    const installCommand = `msiexec /i ${msiPath} /qn`;
+    await execCommand(installCommand);
+
+    taskLib.setResult(taskLib.TaskResult.Succeeded, 'Install Complete')
+  }
+  catch (err) {
+    taskLib.warning(err);
+    taskLib.setResult(taskLib.TaskResult.Failed, err.message);
+  }
+}
+
+run();
